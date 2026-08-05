d407b6424b0eaba57991504ece39cdda77c1526f
Gus <gus@littlebigbyte.com>
2026-08-05 10:11:04 +1000

    Port PiStation to Python 3
    Update the script for Python 3 compatibility by switching the shebang, modernizing input and print calls, and correcting the frequency range check. Also clean up exception handling and formatting.

--- PiStation.py
+++ PiStation.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import os, argparse
 
-parser = argparse.ArgumentParser(prog='python PiStation.py', description='Broadcasts WAV/MP3 file over FM using RPI GPIO #4 pin.')
+parser = argparse.ArgumentParser(prog='python3 PiStation.py', description='Broadcasts WAV/MP3 file over FM using RPI GPIO #4 pin.')
 parser.add_argument("song_file")
 parser.add_argument("-f", "--frequency", help="Set TX frequency. Acceptable range 87.1-108.2", type=float)
 arg = parser.parse_args()
@@ -10,31 +10,31 @@ arg = parser.parse_args()
 def main():
     os.system('clear')
     frequency = 0
-    #frequency=freq()	
-    print ("Welcome to PiStation!  \nVersion 1.0 \nGPLv3 License\n")    
+    #frequency=freq()
+    print("Welcome to PiStation!  \nVersion 1.0 \nGPLv3 License\n")
     #This block is for setting default values for frequency in case argument is not provided
     if arg.frequency is None:
-        frequency = raw_input("Enter the frequency (press Enter to set default frequency of 103.3 MHz) : ")
+        frequency = input("Enter the frequency (press Enter to set default frequency of 103.3 MHz) : ")
         if frequency == "":
             frequency = '103.3'
-    elif 87.1 >= arg.frequency >= 108.2:
-        print "Frequency argument out of range.";exit()
+    elif not (87.1 <= arg.frequency <= 108.2):
+        print("Frequency argument out of range."); exit()
     else:
-	frequency = str(arg.frequency)
-    print frequency
+        frequency = str(arg.frequency)
+    print(frequency)
     try:
         if ".mp3" in arg.song_file.lower():
             os.system("ffmpeg -i "+arg.song_file+" "+"-f s16le -ar 22.05k -ac 1 - | sudo ./fm_transmitter -f"+" "+frequency+" "+" - ")
-        elif ".wav" in arg.song_file.lower():	    
-	    os.system("sudo ./fm_transmitter -f"+" "+frequency+" "+arg.song_file)
+        elif ".wav" in arg.song_file.lower():
+            os.system("sudo ./fm_transmitter -f"+" "+frequency+" "+arg.song_file)
         else:
-            print "That file extension is not supported."
-            print "File name provided: %s" %arg.song_file
+            print("That file extension is not supported.")
+            print("File name provided: %s" % arg.song_file)
             raise IOError
-    except Exception:
-        print "Something went wrong. Halting."; exit()
     except IOError:
-        print "There was an error regarding file selection. Halting."; exit()
-    
+        print("There was an error regarding file selection. Halting."); exit()
+    except Exception:
+        print("Something went wrong. Halting."); exit()
+
 if __name__ == '__main__':
     main()
